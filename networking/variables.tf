1c6cf427523bc23b8e09cda28862e099e61f7df1
Pritam Singh <pritam.singh@intelliswift.com>
2025-05-24 11:55:13 +0000

    Update variables.tf

--- networking/variables.tf
+++ networking/variables.tf
@@ -19,3 +19,15 @@ variable "private_subnet_cidrs" {
   type        = list(string)
   default     = ["10.0.2.0/24"]
 }
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+  default     = "my-vpc"
+}
+
+variable "subnet_name" {
+  description = "Name prefix for the subnets"
+  type        = string
+  default     = "my-subnet"
+}
